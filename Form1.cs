49d1268bb2c34f1fb499e260a9e2c1998473bc05
raph559 <raph.richaud@gmail.com>
2026-02-12 11:28:26 +0100

    feat(core): switched to MVVM Community Toolkit pattern architecture

--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,34 @@
+using OpenTuningTool.ViewModels;
+
 namespace OpenTuningTool;
 
 public partial class Form1 : Form
 {
-    public Form1()
+    private readonly MainViewModel viewModel;
+
+    public Form1(MainViewModel viewModel)
     {
+        this.viewModel = viewModel;
         InitializeComponent();
+        BindViewModel();
+        WireCommands();
+    }
+
+    private void BindViewModel()
+    {
+        txtUserName.DataBindings.Add("Text", viewModel, nameof(MainViewModel.UserName), false, DataSourceUpdateMode.OnPropertyChanged);
+        lblGreeting.DataBindings.Add("Text", viewModel, nameof(MainViewModel.Greeting), false, DataSourceUpdateMode.OnPropertyChanged);
+    }
+
+    private void WireCommands()
+    {
+        btnGenerate.Click += (_, _) => viewModel.GenerateGreetingCommand.Execute(null);
+        btnClear.Click += (_, _) => viewModel.ClearGreetingCommand.Execute(null);
+
+        btnClear.Enabled = viewModel.ClearGreetingCommand.CanExecute(null);
+        viewModel.ClearGreetingCommand.CanExecuteChanged += (_, _) =>
+        {
+            btnClear.Enabled = viewModel.ClearGreetingCommand.CanExecute(null);
+        };
     }
 }
