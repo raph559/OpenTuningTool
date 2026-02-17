66ddb74deeadcc3bcefee01b01806d80c9bd7b69
raph559 <raph.richaud@gmail.com>
2026-02-17 15:12:13 +0100

    feat(core): added new functions and fields to XdfDocument class

--- Models/XdfDocument.cs
+++ Models/XdfDocument.cs
@@ -2,16 +2,40 @@ namespace OpenTuningTool.Models;
 
 class XdfDocument
 {
-	private List<XdfTable> ?_tables;
-	private Dictionary<int, XdfObject> ?_objectsById;
+	private readonly List<XdfTable> _tables;
+	private readonly List<XdfConstant> _constants;
+	private readonly Dictionary<int, XdfObject> _objectsById;
 
-	public IReadOnlyList<XdfTable> ?Tables;
-	public IReadOnlyDictionary<int, XdfObject> ?Objects;
+	public IReadOnlyList<XdfTable> Tables => _tables;
+	public IReadOnlyList<XdfConstant> Constants => _constants;
+	public IReadOnlyDictionary<int, XdfObject> Objects => _objectsById;
 
-	public XdfDocument(){}
+	public XdfDocument()
+	{
+		_tables = new List<XdfTable>();
+		_constants = new List<XdfConstant>();
+		_objectsById = new Dictionary<int, XdfObject>();
+	}
 
 	internal void AddTable(XdfTable table)
 	{
-		if (table == null) return;
+		ArgumentNullException.ThrowIfNull(table);
+
+		if (_objectsById.ContainsKey(table.UniqueId)) 
+			throw new InvalidOperationException("An object with this ID already exists.");
+
+		_tables.Add(table);
+		_objectsById.Add(table.UniqueId, table);
+	}
+
+	internal void AddConstant(XdfConstant constant)
+	{
+		ArgumentNullException.ThrowIfNull(constant);
+
+		if (_objectsById.ContainsKey(constant.UniqueId))
+			throw new InvalidOperationException("An object with this ID already exists.");
+
+		_constants.Add(constant);
+		_objectsById.Add(constant.UniqueId, constant);
 	}
 }
